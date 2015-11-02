078d9fdfa0133e3bb13efccdddd9c13eedd7c7a8
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-11-02 14:43:40 -0500

    Updated from Brython Server: 11/2/2015 2:43:40 PM

--- cryptography.py
+++ cryptography.py
@@ -11,3 +11,5 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 input1 = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+for x in ['a','b','c','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z']
