54aa5ce9920cc973c93fdbabf1f3e7ec2a18cb6f
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-11-04 13:30:13 -0500

    Updated from Brython Server: 11/4/2015 1:30:16 PM

--- cryptography.py
+++ cryptography.py
@@ -11,6 +11,7 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 input1 = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+let/num = [('a', 0),('b', 1),('c', 2),('d', 3),('e', 4),('f', 5),('g', 6),('h', 7),('i', 8),('j', 9),('k', 10),('l', 11),('m', 12),('n', 13),('o', 14),('p', 15),('q', 16),('r', 17),('s', 18),('t', 19),('u', 20),('v', 21),('w', 22),('x', 23),('y', 24),('z', 25),]
 
 while input1 != 'q':
     for x in ['a','b','c','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z']:
